dca538360b64cc677be09ae5571a1049ada051f3
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-12-03 14:03:42 -0500

    Update date1javascript.js

--- clock1/date1javascript.js
+++ clock1/date1javascript.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function updateDate() {
         const dateElement = document.getElementById('date');
         const now2 = new Date();
-        
-        const month = String(now2.getMonth() + 1);
+
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const month = String(now2.getMonth());
+        let month = months[month.getMonth()];
         const day = String(now2.getDate());
         const year = String(now2.getFullYear());
 
