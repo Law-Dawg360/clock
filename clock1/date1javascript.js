f1d7237e485e7ae5353a2e3b65e7b5b8ad4d4728
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-12-03 13:53:00 -0500

    Create date1javascript.js

---
+++ clock1/date1javascript.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function updateDate() {
+        const dateElement = document.getElementById('date');
+        const now2 = new Date();
+        
+        const month = String(now2.getMonth());
+        const day = String(now2.getDate());
+        const year = String(now2.getYear());
+
+        dateElement.textContent = `${month}/${day}/${year}`;
+    }
+
+    setInterval(updateDate, 1000);
+    updateDate();
+});
