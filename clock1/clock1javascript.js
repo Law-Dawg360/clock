fe3d1521f9b393fe01be37b5fa93deef3f3dba1e
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-12-03 13:52:40 -0500

    Update clock1javascript.js

--- clock1/clock1javascript.js
+++ clock1/clock1javascript.js
@@ -13,18 +13,3 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(updateClock, 1000);
     updateClock();
 });
-document.addEventListener('DOMContentLoaded', () => {
-    function updateDate() {
-        const dateElement = document.getElementById('date');
-        const now2 = new Date();
-        
-        const month = String(now2.getMonth());
-        const day = String(now2.getDate());
-        const year = String(now2.getYear());
-
-        dateElement.textContent = `${month}/${day}/${year}`;
-    }
-
-    setInterval(updateDate, 1000);
-    updateDate();
-});
