65e9d260921af551ccff9b5263d98daefa1b345b
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-12-04 08:20:13 -0500

    Update date1javascript.js

--- clock1/date1javascript.js
+++ clock1/date1javascript.js
@@ -3,14 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const dateElement = document.getElementById('date');
         const now2 = new Date();
 
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const monthsAsLong = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const monthLong = monthsAsLong[now2.getMonth()];
+        const months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
         const month = months[now2.getMonth()];
         const day = String(now2.getDate()).padStart(2, '0'); // Ensure two-digit format
         const year = String(now2.getFullYear()).slice(-2); // Get last two digits
 
         dateElement.textContent = `${month}/${day}/${year}`;
         const dateLongFormElement = document.getElementById('dateLongForm');
-        dateLongFormElement.textContent = `${month} ${day}, ${year}`;
+        dateLongFormElement.textContent = `${monthLong} ${day}, ${year}`;
     }
 
     setInterval(updateDate, 1000);
